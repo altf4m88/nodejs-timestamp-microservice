3dea3b497f198c12e98fdd6f6db5514a5d376fe7
altf4m88 <fadhilmauladhani@gmail.com>
2020-12-01 20:46:32 +0700

    uploaded the main code

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:dateparam", (req, res) => {
+  const date = new Date(req.params.dateparam);
+  res.json({"unix" : date.valueOf(), "utc" : date.toUTCString()})
+})
+
 let listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
