c66be4eb1898a4f8999ca8f67c8c95daf8b4ca1f
altf4m88 <fadhilmauladhani@gmail.com>
2020-12-01 21:14:59 +0700

    updated server.js

--- server.js
+++ server.js
@@ -12,10 +12,27 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/:dateparam", (req, res) => {
-  const date = new Date(req.params.dateparam);
+app.get("/api/timestamp/", (req, res) => {
+  let date = new Date(Date.now());
   res.json({"unix" : date.valueOf(), "utc" : date.toUTCString()})
 })
+
+app.get("/api/timestamp/:dateparam", (req, res) => {
+  let dateParam = req.params.dateparam;
+  let unixRegex = /\d{5,}/;
+
+  if(unixRegex.test(dateParam)){
+    let dateInt = parseInt(dateParam);
+    let date = new Date(dateInt);
+
+    res.json({"unix" : dateInt, "utc" : date.toUTCString()})
+  } else {
+    let date = new Date(dateParam);
+    if(date.toString() == "Invalid Date") res.json({error : "Invalid Date"});
+
+    res.json({"unix" : date.valueOf(), "utc" : date.toUTCString()})
+  }
+});
 
 let listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
